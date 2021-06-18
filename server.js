9f296d7dc2b632a6b25fdc5b8b23aae2df060428
joey <doode1524@gmail.com>
2021-06-18 15:27:38 -0400

    configure server.js init

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import mongoose from 'mongoose'
+// App config
+const app = express()
+const port = process.env.PORT || 8001
+
+// Middlewares
+
+// DB config
+
+// Api endpoints
+app.get('/', (req, res) => res.status(200).send('HELLO WORLD!'))
+// Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
