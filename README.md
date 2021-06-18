c21a090affa00751c8913bea756e52d1b0c84da8
joey <doode1524@gmail.com>
2021-06-18 16:33:52 -0400

    configure db and endpoint

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mern-tinder-clone-backend
+nZr6Ef0sJZKEdMRi
+
+// run nodemon server.js to start
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from "express";
+import mongoose from "mongoose";
 // App config
-const app = express()
-const port = process.env.PORT || 8001
-
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://Joey:nZr6Ef0sJZKEdMRi@cluster0.c6o2f.mongodb.net/tinderdb?retryWrites=true&w=majority`;
 // Middlewares
-
 // DB config
-
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 // Api endpoints
-app.get('/', (req, res) => res.status(200).send('HELLO WORLD!'))
+app.get("/", (req, res) => res.status(200).send("HELLO WORLD!"));
 // Listener
-app.listen(port, () => console.log(`listening on localhost: ${port}`))
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
